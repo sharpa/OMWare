f44236656eb313a8e81c5cd7e08ae3c2cd5da990
Aaron Sharp <sharp.aaron.r@gmail.com>
2015-05-26 14:01:37 -0600

    Began framework for code execution testing
    (This is a module, so eventually it won't have a "main method")

---
+++ POMMIO.py
@@ -0,0 +1,9 @@
+# Module: POMMIO
+# Version: 0.1
+# Author: Aaron Sharp
+# Date: 05/26/2015
+# 
+# The purpose of this module is to import and export optical maps
+# from/into a variety of file formats
+
+print("POMMIO imported")
