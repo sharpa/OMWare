6d30da44ed00cb8adcf9ccb4263d81495a9ed09e
Aaron Sharp <sharp.aaron.r@gmail.com>
2015-05-26 16:27:54 -0600

    Added comments for future direction and design

---
+++ Assemble/__init__.py
@@ -0,0 +1,1 @@
+# Assemble refers to a de novo, most likely shotgun strategy assembly of several "optical maps" (individual molecules) into fewer consensus contigs
---
+++ Utils/__init__.py
@@ -0,0 +1,15 @@
+# Common, miscellaneous scripts for optical map manipulation
+
+# CodeWriter
+# This module provides [will provide] encapsulation for platform specific code writing
+# For example, I may want to run my assembly in serial on my Windows laptop (not a great idea),
+# or I may want to run it in serial on a specific flavor of linux based cluster (more likely).
+# It's not the job of the optical map assembly to keep track of OS specifications
+
+# Resources
+# Similar to code writer above, many assembly algorithms are sensitive to hardware availabilities,
+# such as maximum memory or threads
+
+# Wizard
+# I'm envisioning a sort of question and answer command line interface experience that will allow
+# new users to choose input parameter values with confidence.  It will include helpful hints.
