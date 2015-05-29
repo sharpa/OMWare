f175563288262f89bcea7c5174b906b8d6a776c9
Aaron Sharp <sharp.aaron.r@gmail.com>
2015-05-29 10:54:26 -0600

    Finished code for BNG .bnx input, output, and molecule abridging

--- POMMIO.py
+++ POMMIO.py
@@ -8,6 +8,8 @@
 
 class POMMIO:
 	def __init__(self, input_file):
+		i_file=open(input_file, "r")
+		i_file.close()
 		self.input_file=input_file
 
 	def getHeaders(self):
@@ -21,18 +23,98 @@ class POMMIO:
 		return headers
 
 	def parse(self, format):
-		result=[]
-		for i, length in enumerate(xrange(1,100,5)):
-			new_mol=Molecule(i, length)
-			result.append(new_mol)
-		return result
+		return POMMIO_iter(self.input_file)
 	def write(self, molecule, o_file, format):
-		o_file.write("	".join(["0", str(molecule.id), str(molecule.length)]) + "\n")
+
+		o_file.write("	".join(["0", str(molecule.id), str(molecule.length), str(molecule.average_intensity), str(molecule.snr), str(molecule.num_labels), molecule.other_data]) + "\n")
+
+		label_data=["1", "0.0"]
+		label_data.extend([str(i) for i in molecule.labels])
+		o_file.write("	".join(label_data) + "\n")
+
+		quality_one_data=["QX11"]
+		quality_one_data.extend(molecule.qualities_one)
+		o_file.write("	".join(quality_one_data) + "\n")
+
+		quality_two_data=["QX12"]
+		quality_two_data.extend(molecule.qualities_two)
+		o_file.write("	".join(quality_two_data) + "\n")
+
+class POMMIO_iter:
+	def __init__(self, input_file):
+		self.i_file=open(input_file, "r")
+	def __iter__(self):
+		return self
+	def next(self):
+		while True:
+			try:
+				line=self.i_file.readline()
+				if line=='':
+					self.i_file.close()
+					raise StopIteration
+
+				if line[0]=="#":
+					continue
+
+				if line [0] != "0":
+					raise Exception("this file is incorrectly formatted")
+				molecule_data=line.split()
+				new_molecule=Molecule(molecule_data[1], molecule_data[2])
+				new_molecule.average_intensity=molecule_data[3]
+				new_molecule.snr=molecule_data[4]
+				new_molecule.num_labels=molecule_data[5]
+				new_molecule.other_data="	".join(molecule_data[6:len(molecule_data)])
+
+				label_line=self.i_file.readline()
+				if label_line[0] != "1":
+					raise Exception("this file is incorrectly formatted")
+				new_molecule.labels=label_line.split()[2:]
+				
+				quality_line_one=self.i_file.readline()
+				if quality_line_one[0:4] != "QX11":
+					raise Exception("this file is incorrectly formatted")
+				new_molecule.qualities_one=quality_line_one.split()[1:]
+				
+				quality_line_two=self.i_file.readline()
+				if quality_line_two[0:4] != "QX12":
+					raise Exception("this file is incorrectly formatted")
+				new_molecule.qualities_two=quality_line_two.split()[1:]
+
+				return new_molecule
+			except StopIteration:
+				raise
+			except IndexError:
+				raise Exception("this file is incorrectly formatted")
+			except:
+				raise
+
 
 class Molecule:
 	def __init__(self, id, length):
 		self.id=id
 		self.length=length
 
-	def shrink(self, distance):
-		self.length=distance
+	def shrink(self, new_length):
+		self.length=new_length
+
+		num_labels=len(self.labels)
+		last_label_index=-1
+		for i in xrange(num_labels-1, -1, -1):
+			if float(self.labels[i]) <= float(self.length):
+				last_label_index=i
+				break
+			if i==0:
+				last_label_index=0
+
+		quality_one_of_molecule_end=self.qualities_one[num_labels-1]
+		quality_two_of_molecule_end=self.qualities_two[num_labels-1]
+
+		self.labels=self.labels[0:last_label_index]
+		self.qualities_one=self.qualities_one[0:last_label_index]
+		self.qualities_two=self.qualities_two[0:last_label_index]
+
+		self.labels.append(self.length)
+		self.qualities_one.append(quality_one_of_molecule_end)
+		self.qualities_two.append(quality_two_of_molecule_end)
+
+		self.num_labels=last_label_index+1
--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python
 
 # The purpose of this test is to run several assemblies
-#	to assess the effect of different coverage level
+#3	to assess the effect of different coverage level
 #	by randomly removing molecules
 
 
 work_dir="/Users/aaron/Dropbox/Stars/General_BNG/code/POMM_data_dir"
-input_file="all.bnx"
+input_file="all_abridged.bnx"
 
-#for coverage in xrange(90, 0, -10):
-#	print(coverage)
-
-from Utils.Rarefactor import Rarefactor
+#from Utils.Rarefactor import Rarefactor
 from Utils.CD import CD
+#with CD(work_dir):
+#	obj=Rarefactor(input_file)
+#	for cutoff in xrange(9, 0, -1):
+#		obj.generateReducedDataset(float(cutoff)/10)
+
+from POMMIO import POMMIO
 with CD(work_dir):
-	obj=Rarefactor(input_file)
-	for cutoff in xrange(9, 0, -1):
-		obj.generateReducedDataset(float(cutoff)/10)
+	o_file=open("all_abridged.bnx_shrunk", "w")
+	pommio=POMMIO(input_file)
+	for molecule in pommio.parse("bnx"):
+		molecule.shrink(7970.0)
+		pommio.write(molecule, o_file, "bnx")
